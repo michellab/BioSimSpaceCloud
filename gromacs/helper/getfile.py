cf80253e28b194644f8e04d49312d77e6a6471f7
Christopher Woods <chryswoods@gmail.com>
2018-07-20 15:49:48 +0000

    Updated the helper scripts to use the functions in the BioSimSpaceFunction library

--- gromacs/helper/getfile.py
+++ gromacs/helper/getfile.py
@@ -1,27 +1,13 @@
-"""
-This script helps debugging by creating the login and request
-from the .oci/config and hard-coded compartment and bucket names
-"""
 
-import oci
-import json
-import os
+import login
+from BioSimSpaceFunction import ObjectStore as objstore
+
+import datetime
 import sys
 
-
+bucket = login.login()
 
 key = sys.argv[1]
 filename = sys.argv[2]
 
-## Now load up the key and get the bucket/compartment ID
-config = oci.config.from_file()
-compartment = "ocid1.compartment.oc1..aaaaaaaat33j7w74mdyjenwoinyeawztxe7ri6qkfbm5oihqb5zteamvbpzq"
-bucket = "test-gromacs-bucket"
-
-key_lines = open(os.path.expanduser(config["key_file"]), "r").readlines()
-
-del config["key_file"]
-config["key_lines"] = key_lines
-## Config is now in the format used by the system as a key/bucket
-
-
+objstore.get_object_as_file(bucket, key, filename)
