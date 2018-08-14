5fc5259e947f2caccddb1b43724c02688b1aec87
Christopher Woods <chryswoods@gmail.com>
2018-08-14 19:41:37 +0000

    Added in an extra couple of encoding functions to handle sending unicode strings as base64 encoded utf-8 strings

--- Acquire/_function.py
+++ Acquire/_function.py
@@ -12,7 +12,18 @@ from io import BytesIO as _BytesIO
 
 import base64 as _base64
 
-__all__ = [ "call_function", "bytes_to_string", "string_to_bytes" ]
+__all__ = [ "call_function", "bytes_to_string", "string_to_bytes", 
+            "string_to_encoded", "encoded_to_string" ]
+
+def string_to_encoded(s):
+    """Return the passed unicode string encoded to a safely
+       encoded base64 utf-8 string"""
+    return bytes_to_string(s.encode("utf-8"))
+
+def encoded_to_string(b):
+    """Return the passed encoded base64 utf-8 string converted
+       back into a unicode string"""
+    return string_to_bytes(b).decode("utf-8")
 
 def bytes_to_string(b):
     """Return the passed binary bytes safely encoded to
@@ -24,7 +35,10 @@ def bytes_to_string(b):
 
 def string_to_bytes(s):
     """Return the passed base64 utf-8 encoded binary data
-       back converted from a string back to bytes"""
+       back converted from a string back to bytes. Note that
+       this can only convert strings that were encoded using
+       bytes_to_string - you cannot use this to convert 
+       arbitrary strings to bytes"""
     if s is None:
         return None
     else:
