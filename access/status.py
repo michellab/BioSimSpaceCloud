29ab5fdb8136d2190b2af93b7370b707d1716bda
Christopher Woods <chryswoods@gmail.com>
2018-08-16 10:53:34 +0000

    Added in the files to create the default 'status' function for the access service

---
+++ access/status.py
@@ -0,0 +1,26 @@
+
+import json
+import fdk
+import oci
+
+import os
+
+from Acquire import ObjectStore
+from accessaccount import loginToAccessAccount
+
+def handler(ctx, data=None, loop=None):
+    """This function gets the status of the access service"""
+
+    try:
+        # The first step is to log into the primary access account.
+        access_client = loginToAccessAccount()
+
+        return json.dumps( {"status" : str(0)} ).encode("utf-8")
+
+    except Exception as e:
+        response = { "error" : str(e) }
+        return json.dumps(response).encode("utf-8")
+
+if __name__ == "__main__":
+    from fdk import handle
+    handle(handler)
