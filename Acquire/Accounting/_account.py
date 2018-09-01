ed8c9a387a6d9e73fadb3c5691cb7e87e689fab8
Christopher Woods <chryswoods@gmail.com>
2018-09-01 17:24:33 +0100

    Staging all for test framework

--- Acquire/Accounting/_account.py
+++ Acquire/Accounting/_account.py
@@ -8,13 +8,59 @@ from Acquire.Service import login_to_service_account \
                         as _login_to_service_account
 from Acquire.ObjectStore import ObjectStore as _ObjectStore
 
+from ._transaction import Transaction as _Transaction
 from ._errors import AccountError, InsufficientFundsError
 
-__all__ = ["Account"]
+__all__ = ["Account", "LineItem"]
 
 
 def _account_root():
     return "accounts"
+
+
+class Authorisation:
+    """This class holds the information needed to authorise a transaction
+       in an account
+    """
+    pass
+
+
+class LineItem:
+    """This class holds the data for a line item in the account. This holds
+       basic information about the item, e.g. its UID, timestamp,
+       authorisation etc.
+    """
+    def __init__(self, uid=None, timestamp=None, authorisation=None):
+        self._uid = uid
+        self._timestamp = timestamp
+        self._authorisation = authorisation
+
+    def is_null(self):
+        """Return whether or not this is a null line item"""
+        return self._uid is None
+
+    def to_data(self):
+        """Return this object as a dictionary that can be serialised to json"""
+        data = {}
+
+        if not self.is_null():
+            data["uid"] = self._uid
+            data["timestamp"] = self._timestamp
+            data["authorisation"] = self._authorisation
+
+        return data
+
+    @staticmethod
+    def from_data(data):
+        """Return a LineItem constructed from the json-decoded dictionary"""
+        l = LineItem()
+
+        if (data and len(data) > 0):
+            l._uid = data["uid"]
+            l._timestamp = data["timestamp"]
+            l._authorisation = data["authorisation"]
+
+        return l
 
 
 class Account:
@@ -56,6 +102,9 @@ class Account:
         self._overdraft_limit = 0
         self._maximum_daily_limit = None
 
+        # make sure that this is saved to the object store
+        self._save_account()
+
     def is_null(self):
         """Return whether or not this is a null account"""
         return self._uid is None
@@ -85,18 +134,21 @@ class Account:
 
         return "%s/%s" % (_account_root(), self.uid())
 
-    def _load_account(self):
+    def _load_account(self, bucket=None):
         """Load the current state of the account from the object store"""
         if self.is_null():
             return
 
-        bucket = _login_to_service_account()
+        if bucket is None:
+            bucket = _login_to_service_account()
+
         data = _ObjectStore.get_object_from_json(bucket, self.key())
         self.__dict__ = _copy(Account.from_data(data).__dict__)
 
-    def _save_account(self):
+    def _save_account(self, bucket=None):
         """Save this account back to the object store"""
-        bucket = _login_to_service_account()
+        if bucket is None:
+            bucket = _login_to_service_account()
         _ObjectStore.set_object_from_json(bucket, self.key(), self.to_data())
 
     def to_data(self):
@@ -128,6 +180,14 @@ class Account:
 
         return account
 
+    def assert_valid_authorisation(self, authorisation):
+        """Assert that the passed authorisation is valid for this
+           account
+        """
+        if not isinstance(authorisation, Authorisation):
+            raise TypeError("The passed authorisation must be an "
+                            "Authorisation")
+
     def _debit(self, transaction, authorisation, bucket=None):
         """Debit the value of the passed transaction  from this account based
            on the authorisation contained
@@ -149,7 +209,11 @@ class Account:
         if self.is_null() or transaction.value() <= 0:
             return None
 
-        # self.assert_valid_authorisation(authorisation)
+        if not isinstance(transaction, _Transaction):
+            raise TypeError("The passed transaction must be a Transaction!")
+
+        self.assert_valid_authorisation(authorisation)
+
         if bucket is None:
             bucket = _login_to_service_account()
 
@@ -170,26 +234,32 @@ class Account:
             _time.sleep(5)
             now = _datetime.datetime.now()
 
+        # we need to record the exact timestamp of this debit...
         timestamp = now.timestamp()
 
+        # and to create a key to find this debit later. The key is made
+        # up from the date of the debit and a random string
         day_key = "%4d-%02d-%02d" % (now.year, now.month, now.day)
-
         uid = "%s/%s" % (day_key, _uuid.uuid4())
 
-        obj_key = "%s/DR:%s" % (uid, transaction.value_string())
+        # the key in the object store is a combination of the key for this
+        # account plus the uid for the debit plus the actual debit value.
+        # We record the debit value in the key so that we can accumulate
+        # the balance from just the key names
+        item_key = "%s/%s/DR:%s" % (self.key(), uid,
+                                    transaction.value_string())
 
-        # write this debit to the object store
-        data = {"timestamp": timestamp,
-                "uid": uid,
-                "authorisation": authorisation.to_data()}
+        # create a line_item for this debit and save it to the object store
+        line_item = LineItem(uid, timestamp, authorisation)
 
-        _ObjectStore.set_object_from_json(bucket, obj_key, data)
+        _ObjectStore.set_object_from_json(bucket, item_key,
+                                          line_item.to_data())
 
         if self.is_beyond_overdraft_limit(bucket):
             # This transaction has helped push the account beyond the
             # overdraft limit. Delete the transaction and raise
             # an InsufficientFundsError
-            _ObjectStore.delete_object(bucket, obj_key)
+            _ObjectStore.delete_object(bucket, item_key)
             raise InsufficientFundsError(
                 "You cannot debit '%s' from account %s as there "
                 "are insufficient funds in this account." %
