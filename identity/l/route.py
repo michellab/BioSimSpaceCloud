56f0c057daf0ad10ab493775edd74d07e925ad93
Christopher Woods <chryswoods@gmail.com>
2018-10-25 15:19:09 -0700

    Working on route.py

--- identity/l/route.py
+++ identity/l/route.py
@@ -1,14 +1,15 @@
 
 from Acquire.Service import unpack_arguments, get_service_private_key
-from Acquire.Service import create_return_value, pack_return_value, \
-                            start_profile, end_profile
+
+import fdk
+
+from fdk import response
 
 
 def handler(ctx, data=None, loop=None):
-    """This function routes calls to sub-functions, thereby allowing
-       a single identity function to stay hot for longer"""
-
-    pr = start_profile()
+    """This function routes to various dynamic html pages that
+       are needed by the identity service
+    """
 
     args = unpack_arguments(data, get_service_private_key)
 
@@ -21,33 +22,6 @@ def handler(ctx, data=None, loop=None):
         if function is None:
             from root import run as _root
             result = _root(args)
-        elif function == "request_login":
-            from request_login import run as _request_login
-            result = _request_login(args)
-        elif function == "get_keys":
-            from get_keys import run as _get_keys
-            result = _get_keys(args)
-        elif function == "get_status":
-            from get_status import run as _get_status
-            result = _get_status(args)
-        elif function == "login":
-            from login import run as _login
-            result = _login(args)
-        elif function == "logout":
-            from logout import run as _logout
-            result = _logout(args)
-        elif function == "register":
-            from register import run as _register
-            result = _register(args)
-        elif function == "request_login":
-            from request_login import run as _request_login
-            result = _request_login(args)
-        elif function == "setup":
-            from setup import run as _setup
-            result = _setup(args)
-        elif function == "whois":
-            from whois import run as _whois
-            result = _whois(args)
         else:
             result = {"status": -1,
                       "message": "Unknown function '%s'" % function}
@@ -56,9 +30,12 @@ def handler(ctx, data=None, loop=None):
         result = {"status": -1,
                   "message": "Error %s: %s" % (e.__class__, str(e))}
 
-    end_profile(pr, result)
-
-    return pack_return_value(result, args)
+    return response.RawResponse(
+        ctx,
+        status_code=200,
+        headers={},
+        response_data=str(result)
+    )
 
 
 if __name__ == "__main__":
