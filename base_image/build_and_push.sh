3fd63fe43c60b40747643ab7523377763d849a53
Christopher Woods <chryswoods@gmail.com>
2018-10-26 03:54:18 +0000

    I've moved everything over to the working updated base image :-)

--- base_image/build_and_push.sh
+++ base_image/build_and_push.sh
@@ -3,6 +3,6 @@
 rsync -a --verbose ../Acquire . --exclude '__pycache__'
 
 docker build -t chryswoods/acquire-base:latest .
-#docker push chryswoods/acquire-base:latest
+docker push chryswoods/acquire-base:latest
 
 #docker run --rm -it chryswoods/acquire-base:latest
