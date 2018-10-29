b5fbdb1ae9ba429ae764f5a7df61d72c8efee53b
Christopher Woods <chryswoods@gmail.com>
2018-10-29 17:35:34 +0000

    Got otpcode page working and can now generate all required login data...

--- identity/html/login_pages.js
+++ identity/html/login_pages.js
@@ -5,7 +5,7 @@
 
 var json_login_data = {};
 
-var pages = ["login-page", "otp-page", "progress-page", "test-page"];
+var pages = ["login-page", "otpcode-page", "progress-page", "test-page"];
 
 /** Function used to switch views between different pages */
 function show_page(new_page){
@@ -30,7 +30,8 @@ function show_page(new_page){
 }
 
 /** Shortcut function to switch back to the login page */
-function show_login_page(){
+function reset_login_page(){
+    json_login_data["otpcode"] = null;
     show_page("login-page");
 }
 
@@ -38,10 +39,10 @@ function show_login_page(){
 function render_test_page(){
     document.write(
         '<div class="results">\
-          <h2 class="results__heading">Form Data</h2>\
+          <h2 class="results__heading">JSON data to be submitted to server...</h2>\
           <pre class="results__display-wrapper"><code class="results__display"></code></pre>\
-          <button type="button" onclick="show_login_page()">Back</button>\
-          <button type="button" onclick="perform_login()">Submit</button>\
+          <button type="button" onclick="reset_login_page()">Back</button>\
+          <button type="button" onclick="perform_login_submit()">Submit</button>\
           </div>');
 }
 
@@ -49,7 +50,66 @@ function render_test_page(){
  *  of from secure storage
  */
 function get_otpcode(){
-    json_login_data["otpcode"] = "56789";
+
+    //can't get the otpcode from local storage so have to ask
+    //the user to supply it
+    if (!json_login_data["otpcode"]){
+        show_page("otpcode-page");
+        return;
+    }
+
+    perform_login();
+}
+
+/** This function is used to submit the login data to the server,
+ *  without first showing testing data
+ */
+function perform_login_submit(){
+
+    // ...this is where we’d actually do something with the form data...
+    // I WANT TO ENCRYPT THE JSON AND SEND IT AS A POST TO THE IDENTITY SERVER
+    var server_public_key = "PUBLIC KEY INSERTED INTO DOCUMENT BY SERVER";
+    var args_json = JSON.stringify(json_login_data);
+    //var encrypted_json = server_public_key.encrypt(args_json);
+
+    // now generate a new pub/priv keypair to encrypt the server call...
+
+    // now call the service by posing the encrypted_json and waiting for
+    // the result...
+    console.log(identity_service_url);
+    console.log(args_json);
+
+    /*fetch(identity_service_url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });*/
+
+    // if remember_device then encrypt the returned otpsecret using the
+    // user's password (we need a secret to keep it safe in the cookiestore)
+
+    // now tell the user whether or not they were successful, and that they
+    // can now close this window (or click a link to try again)
+
+}
+
+/** This function is used to submit the login data to the server */
+function perform_login(){
+
+    // Testing only: print the form data onscreen as a formatted JSON object.
+    if (isTesting()){
+        var dataContainer = document.getElementsByClassName('results__display')[0];
+
+        // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+        dataContainer.textContent = JSON.stringify(json_login_data, null, "  ");
+
+        show_page("test-page");
+        return;
+    }
+
+    perform_login_submit();
 }
 
 /* This function renders the login page */
@@ -63,7 +123,7 @@ function render_login_page(){
         <p class="content__lede">\
         (unique session ID ' + getSessionUID() + ')\
         </p>\
-        <form class="content__form contact-form">\
+        <form class="content__form contact-form" id="user-login-form">\
         <div class="contact-form__input-group">\
             <label class="contact-form__label" for="username">\
               username\
@@ -83,12 +143,6 @@ function render_login_page(){
                 autofill="password"/>\
         </div>\
         <button class="contact-form__button" type="submit">Login</button>\
-        <div class="contact-form__input-group">\
-            <p class="contact-form__label--checkbox-group">Remember device</p>\
-            <input class="contact-form__input contact-form__input--checkbox"\
-                id="remember_device" name="remember_device" type="checkbox"\
-                value="true"/>\
-        </div>\
       </form>');
 
     /**
@@ -118,7 +172,7 @@ function render_login_page(){
         }
 
         if (data["password"]){
-            json_login_data["password"];
+            json_login_data["password"] = data["password"];
             var remind_input = document.getElementById("contact-form__remind-password");
             remind_input.style.display = "none";
         }
@@ -136,56 +190,83 @@ function render_login_page(){
         json_login_data["function"] = "login";
         json_login_data["short_uid"] = getSessionUID();
 
-        // now try to get the one-time-code
+        // now try to get the one-time-code - this will move onto the
+        // next stage of the process...
         get_otpcode();
-
-        // Testing only: print the form data onscreen as a formatted JSON object.
-        if (isTesting()){
-            var dataContainer = document.getElementsByClassName('results__display')[0];
-
-            // Use `JSON.stringify()` to make the output valid, human-readable JSON.
-            dataContainer.textContent = JSON.stringify(json_login_data, null, "  ");
-
-            show_page("test-page");
-            return;
-        }
-
-        // ...this is where we’d actually do something with the form data...
-        // I WANT TO ENCRYPT THE JSON AND SEND IT AS A POST TO THE IDENTITY SERVER
-        var server_public_key = "PUBLIC KEY INSERTED INTO DOCUMENT BY SERVER";
-        var args_json = JSON.stringify(data);
-        //var encrypted_json = server_public_key.encrypt(args_json);
-
-        // now generate a new pub/priv keypair to encrypt the server call...
-
-        // now call the service by posing the encrypted_json and waiting for
-        // the result...
-        console.log(identity_service_url);
-        console.log(args_json);
-
-        /*fetch(identity_service_url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-            console.log(JSON.stringify(myJson));
-        });*/
-
-        // if remember_device then encrypt the returned otpsecret using the
-        // user's password (we need a secret to keep it safe in the cookiestore)
-
-        // now tell the user whether or not they were successful, and that they
-        // can now close this window (or click a link to try again)
     };
 
-    var form = document.getElementsByClassName('contact-form')[0];
+    var form = document.getElementById('user-login-form');
     form.addEventListener('submit', handleFormSubmit);
 }
 
 /** This function renders the device page */
-function render_device_page()
+function render_otpcode_page()
 {
-    document.write('<p>Device login page</p>');
+    document.write(
+        '<h1 class="content__heading">\
+            <a href="' + getIdentityServiceURL() + '">\
+                Logging into Acquire\
+            </a>\
+        </h1>\
+        <p class="content__lede">\
+        (unique session ID ' + getSessionUID() + ')\
+        </p>\
+        <form class="content__form contact-form" id="user-otpcode-form">\
+        <div class="contact-form__input-group">\
+            <label class="contact-form__label" for="otpcode">\
+            2-step Verification - \
+            Get a verification code from your Authenticator app \
+              <span class="contact-form__remind-input" id="contact-form__remind-otpcode"> (*) you must supply a 2-step verification code</span>\
+              </label>\
+            <input class="contact-form__input contact-form__input--text"\
+                id="otpcode" name="otpcode"\
+                type="text"/>\
+        </div>\
+        <div class="contact-form__input-group">\
+            <p class="contact-form__label--checkbox-group">Remember device</p>\
+            <input class="contact-form__input contact-form__input--checkbox"\
+                id="remember_device" name="remember_device" type="checkbox"\
+                value="true"/>\
+        </div>\
+        <button class="contact-form__button" type="submit">Login</button>\
+      </form>');
+
+    /**
+     * A handler function to prevent default submission and run our custom script.
+     * @param  {Event} event  the submit event triggered by the user
+     * @return {void}
+    */
+    var handleFormSubmit = function handleFormSubmit(event) {
+        // Stop the form from submitting since we’re handling that with AJAX.
+        event.preventDefault();
+
+        // Call our function to get the form data.
+        var data = formToJSON(form.elements);
+
+        var all_ok = 1;
+
+        // make sure that we have everything we need...
+        if (data["otpcode"]){
+            json_login_data["otpcode"] = data["otpcode"];
+            var remind_input = document.getElementById("contact-form__remind-otpcode");
+            remind_input.style.display = "none";
+        }
+        else{
+            var remind_input = document.getElementById("contact-form__remind-otpcode");
+            remind_input.style.display = "inline";
+            all_ok = 0;
+        }
+
+        json_login_data["remember_device"] = data["remember_device"];
+
+        if (all_ok)
+        {
+            perform_login();
+        }
+    };
+
+    var form = document.getElementById('user-otpcode-form');
+    form.addEventListener('submit', handleFormSubmit);
 }
 
 /** This function renders the progress page */
