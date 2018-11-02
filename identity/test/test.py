fff1d4fd5f6efb26cf3670a09b064334e87abe52
Christopher Woods <chryswoods@gmail.com>
2018-11-02 15:17:24 +0000

    Test function is useful for debugging decryption

--- identity/test/test.py
+++ identity/test/test.py
@@ -7,6 +7,8 @@ from Acquire.Service import unpack_arguments, get_service_private_key
 from Acquire.Service import create_return_value, pack_return_value, \
                             start_profile, end_profile
 
+from Acquire.ObjectStore import string_to_bytes
+
 from Acquire.Crypto import PrivateKey, PublicKey
 
 async def handler(ctx, data=None, loop=None):
@@ -22,10 +24,16 @@ async def handler(ctx, data=None, loop=None):
 
         privkey = get_service_private_key()
 
-        encrypted = data["data"]
-        plain = privkey.decrypt(encrypted)
+        #encrypted = string_to_bytes("eMWVILuh1HzwTskWmA3acSi/ZKjmo+iwUiBo0eLC71BdfK4zo4zkRorRFD3Fj2Tw86TbcevF1VLHVTXQK73lDtJsUJSTnYQjSHCb8eIk/AaBWlQkzwQhmKeO+f5WCcYSmCOyad/CftZ2QwZxgDBuXF2z7Aa0VlWwYMknp/OoUPMojgHZb3kVkaay+gVvGfeIWHtFfpPHy7ckN3Kijm74GMl8IBLpSLGoSp8PJRakwKPutIgVJR3RRCEvn19Hl6bmoHHnVTyTbGkPcig7Y2XiMSr6ohfiHGJw10PVNEgk2u3oSq9CuMawWSRiCDhuIttzbz/fSwJ70JS5DONqpdG+Gg==")
+        encrypted = string_to_bytes(data["data"])
 
-        result["message"] = plain
+        decrypted = privkey.decrypt(encrypted)
+
+        result["original"] = data["data"]
+        result["encrypted"] = ", ".join( [str(x) for x in list(encrypted)] )
+        result["message"] = "testing"
+        result["decrypted"] = str(decrypted)
+        result["status"] = -1
 
     except Exception as e:
         message = {"status": -1,
@@ -39,9 +47,4 @@ async def handler(ctx, data=None, loop=None):
     return json.dumps(result)
 
 if __name__ == "__main__":
-    try:
-        fdk.handle(handler)
-    except Exception as e:
-        print({"message": "Error! %s" % str(e), "status": -1})
-    except:
-        print({"message": "Unknown error!", "status": -1})
+    fdk.handle(handler)
